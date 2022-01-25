d9cf50c1ab6862e0c708692271989f043f293dd5
Rajiofficial <98094600+Rajiofficial@users.noreply.github.com>
2022-01-25 19:03:21 +0530

    Add files via upload

---
+++ day1.js
@@ -0,0 +1,32 @@
+let a=typeof(1);
+console.log(a);
+
+let b=typeof(1.1);
+console.log(b);
+
+let c=typeof("1.1");
+console.log(c);
+
+let d=typeof(true);
+console.log(d);
+
+let e=typeof(null);
+console.log(e);
+
+let f=typeof(undefined);
+console.log(f);
+
+let g=typeof([]);
+console.log(g);
+
+let h=typeof(NaN);
+console.log(h);
+
+let i=typeof({});
+console.log(i);
+
+
+
+
+
+
